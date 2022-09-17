a55c4b3616fea1a1440034afb48bceaeac14d23c
DoMess <michael.lam14@outlook.com>
2022-09-17 17:34:45 +1000

    Build 2
    Updated PolygonPlus to include polygons with more sides up to 20 (Icosagon).

--- src/PolygonPlus.java
+++ src/PolygonPlus.java
@@ -26,6 +26,24 @@ public class PolygonPlus implements Polygon {
                 return (5 + Math.sqrt(5)) * (sideLength * sideLength);
             case 11: // Hendecagon
                 return (11 / 4) * (Math.sqrt(5 + 2 * Math.sqrt(5))) * (sideLength * sideLength);
+            case 12: // Dodecagon
+                return (3 + Math.sqrt(5)) * (sideLength * sideLength);
+            case 13: // Tridecagon
+                return (13 / 4) * (Math.sqrt(5 + 2 * Math.sqrt(5))) * (sideLength * sideLength);
+            case 14: // Tetradecagon
+                return (7 + Math.sqrt(5)) * (sideLength * sideLength);
+            case 15: // Pentadecagon
+                return (15 / 4) * (Math.sqrt(5 + 2 * Math.sqrt(5))) * (sideLength * sideLength);
+            case 16: // Hexadecagon
+                return (4 + Math.sqrt(5)) * (sideLength * sideLength);
+            case 17: // Heptadecagon
+                return (17 / 4) * (Math.sqrt(5 + 2 * Math.sqrt(5))) * (sideLength * sideLength);
+            case 18: // Octadecagon
+                return (9 + Math.sqrt(5)) * (sideLength * sideLength);
+            case 19: // Enneadecagon
+                return (19 / 4) * (Math.sqrt(5 + 2 * Math.sqrt(5))) * (sideLength * sideLength);
+            case 20: // Icosagon
+                return (10 + Math.sqrt(5)) * (sideLength * sideLength);
             default:
                 return 0;
         }
